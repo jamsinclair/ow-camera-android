03deb44c08360144523ed2a874979d65c6d44973
jamsinclair <jamsinclairnz@gmail.com>
2016-06-06 00:50:56 +1200

    Rename namespace for build

---
+++ src/com/github/jamsinclair/owcamera/MyDebug.java
@@ -0,0 +1,8 @@
+package com.github.jamsinclair.owcamera;
+
+/** Global constant to control logging, should always be set to false in
+ *  released versions.
+ */
+public class MyDebug {
+	public static final boolean LOG = false;
+}
