c5b6a6558683b112993e9af28ea94914b919fc05
jamsinclair <jamsinclairnz@gmail.com>
2017-03-12 22:55:40 +1300

    Tweak rename script

--- rename-namespace.sh
+++ rename-namespace.sh
@@ -4,6 +4,7 @@ export LC_CTYPE=C
 export LANG=C
 
 find ./app/src/main/java -type f -print0 | xargs -0 sed -i '' 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera/g'
+find ./app/src/main/rs -type f -print0 | xargs -0 sed -i '' 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera/g'
 find ./app/src/main/java -type f -print0 | xargs -0 sed -i '' 's/Open Camera/OW Camera for Pebble/g'
 find ./app/src/main/res/values* -type f -print0 | xargs -0 sed -i '' 's/Open Camera/OW Camera for Pebble/g'
 
