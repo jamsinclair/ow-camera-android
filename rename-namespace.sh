c7c68c0735c21a4777f5b09dc2f6676d9139dacf
Jamie Sinclair <jamsinclairnz@gmail.com>
2026-01-12 19:47:52 +0900

    add rename namespace script

---
+++ rename-namespace.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export LC_CTYPE=C
+export LANG=C
+
+find ./app/src/main/java -type f -print0 | xargs -0 sed -i '' 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera2/g'
+find ./app/src/main/java -type f -print0 | xargs -0 sed -i '' 's/Open Camera/OW Camera 2 for Pebble/g'
+find ./app/src/main/res/values* -type f -print0 | xargs -0 sed -i '' 's/Open Camera/OW Camera 2 for Pebble/g'
+
+perl -pi -e 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera2/g' ./app/src/main/AndroidManifest.xml
+perl -pi -e 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera2/g' ./app/build.gradle
+
+mkdir -p ./app/src/main/java/com
+mkdir -p ./app/src/main/java/com/github
+mkdir -p ./app/src/main/java/com/github/jamsinclair
+
+mv ./app/src/main/java/net/sourceforge/opencamera ./app/src/main/java/com/github/jamsinclair
+mv ./app/src/main/java/com/github/jamsinclair/opencamera ./app/src/main/java/com/github/jamsinclair/owcamera2
+
+rm -rf ./app/src/main/java/net
