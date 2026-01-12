0ebab6db24619ba3996dc7e9fa7b63c92eabfb03
Jamie Sinclair <jamsinclairnz@gmail.com>
2026-01-12 20:20:53 +0900

    refresh launcher icon assets

---
+++ copy-assets.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp ~/Downloads/xxxhdpi.png app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
+cp ~/Downloads/xxhdpi.png app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+cp ~/Downloads/xhdpi.png app/src/main/res/mipmap-xhdpi/ic_launcher.png
+cp ~/Downloads/hdpi.png app/src/main/res/mipmap-hdpi/ic_launcher.png
+cp ~/Downloads/mdpi.png app/src/main/res/mipmap-mdpi/ic_launcher.png
