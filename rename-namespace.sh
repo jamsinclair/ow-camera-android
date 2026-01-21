1491a161d3ae03452f13a0596eb62871e1f5c988
Jamie Sinclair <jamsinclairnz@gmail.com>
2026-01-21 23:44:12 +0900

    fix rename script

--- rename-namespace.sh
+++ rename-namespace.sh
@@ -4,17 +4,21 @@ export LC_CTYPE=C
 export LANG=C
 
 find ./app/src/main/java -type f -print0 | xargs -0 sed -i '' 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera2/g'
+find ./app/src/main/kotlin -type f -print0 | xargs -0 sed -i '' 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera2/g'
 find ./app/src/main/java -type f -print0 | xargs -0 sed -i '' 's/Open Camera/OW Camera 2 for Pebble/g'
 find ./app/src/main/res/values* -type f -print0 | xargs -0 sed -i '' 's/Open Camera/OW Camera 2 for Pebble/g'
 
 perl -pi -e 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera2/g' ./app/src/main/AndroidManifest.xml
 perl -pi -e 's/net.sourceforge.opencamera/com.github.jamsinclair.owcamera2/g' ./app/build.gradle
 
-mkdir -p ./app/src/main/java/com
-mkdir -p ./app/src/main/java/com/github
 mkdir -p ./app/src/main/java/com/github/jamsinclair
+mkdir -p ./app/src/main/kotlin/com/github/jamsinclair
 
 mv ./app/src/main/java/net/sourceforge/opencamera ./app/src/main/java/com/github/jamsinclair
 mv ./app/src/main/java/com/github/jamsinclair/opencamera ./app/src/main/java/com/github/jamsinclair/owcamera2
 
+mv ./app/src/main/kotlin/net/sourceforge/opencamera ./app/src/main/kotlin/com/github/jamsinclair
+mv ./app/src/main/kotlin/com/github/jamsinclair/opencamera ./app/src/main/kotlin/com/github/jamsinclair/owcamera2
+
 rm -rf ./app/src/main/java/net
+rm -rf ./app/src/main/kotlin/net
